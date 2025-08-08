ec65b61bbe4a2b0d3df9b356ccc93e5a36272f20
Sunil Sharma <Arnil1718@gmail.com>
2025-08-08 11:39:25 +0545

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import os
 API_ID = os.environ.get("API_ID", "YOUR_API_ID")
 API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
-SESSION_STRING = os.environ.get("SESSION_STRING", None) # Put your session string here if needed
-
-GOFILE_TOKEN = os.environ.get("dOZEVe1fA6tO2pTENQ21fKsp9zoiPRFQ") 
+SESSION_STRING = os.environ.get("SESSION_STRING", None) # Put your session string here if needed 
+GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN", "dOZEVe1fA6tO2pTENQ21fKsp9zoiPRFQ") 
