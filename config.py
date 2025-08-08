cdc57febee27fe1eaf8a9b70ce5071971f24af01
Sunil Sharma <Arnil1718@gmail.com>
2025-08-08 07:57:59 +0545

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = "YOUR_API_ID"
+API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+# Optional for userbot/premium support
+SESSION_STRING = None  # Put your session string here if needed
+
+Gofile_API = "https://api.gofile.io/uploadFile"
